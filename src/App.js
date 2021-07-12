0d9b1a39e268f70a215b429e09bf956b667208e6
Doohan AS <79241754+doohanas@users.noreply.github.com>
2021-07-12 08:52:32 +0700

    add .env spotify setting and add axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
 
 function App() {
+  console.log(process.env.REACT_APP_GIPHY);
   return (
     <div className="App">
       <header className="App-header">
@@ -23,3 +25,11 @@ function App() {
 }
 
 export default App;
+
+// Client ID = 60f93733c8844192821a380b56f57304
+// Client Secret ddc58c9a1cdd44028b66ebba97d6abc6
+// Redirect URIs http://localhost:8888/callback
+
+// 1. Req Authorization
+// GET https://accounts.spotify.com/authorize
+
