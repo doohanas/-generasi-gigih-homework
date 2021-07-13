2a7daa2398960bf0d140a1eaa67c289efa8c19b7
Doohan AS <79241754+doohanas@users.noreply.github.com>
2021-07-13 22:06:38 +0700

    Init homework generasi gigih v0.2.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data/data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>My Playlist</h1>
+      <div className="container-playlist">
+        <img src={data.album.images[1].url} alt="Queen"></img>
+        <p id="playlist-title">{data.album.name}</p> 
+        <p id="playlist-artist">{data.album.artists[0].name}</p> 
+        <p id="playlist-album">{data.album.album_type}</p>
+        <button id="playlist-btn" type="button">Select</button>
+        </div>
+     </div>
   );
 }
 
